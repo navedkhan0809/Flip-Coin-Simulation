e1bdf7922d18561f182ce5d156d2cbfebff818c8
Naved Khan <navedkhan0809@gmail.com>
2021-03-27 13:42:15 +0530

    Added: Flip coin till either of them have got 21 times

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -3,11 +3,10 @@
 i=0
 j=0
 k=0
+count1=0
+count2=0
 
-echo -e "Enter Number of times of a Coin flips=\c"
-read f
-
-while [ $i -lt $f ]
+while [ $i -lt 40 ]
 do
 	toss=$((RANDOM%2))
 	HEADS=1
@@ -15,11 +14,38 @@ do
 	if [ $toss -eq $HEADS ]
 	then
    	j=$(($j+1))
+		if [ $j == 21 ]
+		then
+			a=`echo "You won with HEADS"`
+			((count1++))
+			y=$k
+		fi
 	else
    	k=$(($k+1))
+		if [ $k == 21 ]
+		then
+			b=`echo "You won with TAILS"`
+			((count2++))
+			z=$j
+		fi
 	fi
 i=$(($i + 1))
 done
 
-echo "You won HEADS $j times"
-echo "You won TAILS $k times"
+if [ $count1 == $count2 ]
+then
+	echo "Its tie"
+else
+	if [ $count1 == 1 ]
+	then
+		echo $a
+		diff1=$((21 - $y))
+		echo "You won by $diff1"
+
+	elif [ $count2 == 1 ]
+	then
+		echo $b
+		diff2=$((21 - $z))
+		echo "You won by $diff2"
+	fi
+fi
